0f9f21ee2d367ec88b6db34d3639cedaab8f26a0
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2018-11-24 21:36:22 +0100

    add a floating animation to the loading screen

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,5 +1,28 @@
 import React from "react";
+import styled from "react-emotion";
 
-const Loading = () => <h1>Loading...</h1>;
+import Layout from '../components/Layout';
+
+import astronaut from "../assets/astronaut.png";
+
+const FloatingDiv = styled('div')`
+  max-width: 100%;
+  max-height: 100%;
+  animation: floating 6s infinite ease-in-out;
+
+  @keyframes floating {
+    0% { transform: translateY(0) }
+    50% { transform: translateY(-20px) }
+    100% { transform: translateY(0) }
+  }
+`;
+
+const Loading = () => (
+  <Layout distributed spanned narrow >
+    <FloatingDiv>
+      <img src={astronaut} alt="" />
+    </FloatingDiv>
+  </Layout>
+);
 
 export default Loading;
