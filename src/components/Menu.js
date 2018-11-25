8bed6dbead4289c227af06e8cdae7bc7ae914196
Poe <piotr.a.ptak@icloud.com>
2018-11-25 10:04:33 +0100

    Make menu scrollable

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,13 +1,13 @@
-import React from "react";
-import styled from "react-emotion";
-import { Link, Redirect } from "react-router-dom";
-import cross from "../assets/cross.svg";
-import Button from "./Button";
-import IconButton from "./IconButton";
-import Layout from "./Layout";
-import useCookie from "../hooks/useCookie";
+import React from 'react';
+import styled from 'react-emotion';
+import { Link, Redirect } from 'react-router-dom';
+import cross from '../assets/cross.svg';
+import Button from './Button';
+import IconButton from './IconButton';
+import Layout from './Layout';
+import useCookie from '../hooks/useCookie';
 
-const MenuBox = styled("nav")`
+const MenuBox = styled('nav')`
   background: ${props => props.theme.color.accent.primary.base};
   position: fixed;
   top: 0;
@@ -25,7 +25,7 @@ const MenuBox = styled("nav")`
   `}
 `;
 
-const Overlay = styled("div")`
+const Overlay = styled('div')`
   transition: background 0.5s ease;
   display: none;
   ${props =>
@@ -34,6 +34,7 @@ const Overlay = styled("div")`
     display: block;
     cursor: pointer;
     position: fixed;
+    overflow: scroll;
     top: 0;
     left: 0;
     right: 0;
@@ -43,7 +44,7 @@ const Overlay = styled("div")`
   `}
 `;
 
-const MenuUl = styled("ul")`
+const MenuUl = styled('ul')`
   margin: 15vh 0;
   padding: 0;
   list-style-type: none;
@@ -66,55 +67,55 @@ const MenuLink = styled(Link)`
   }
 `;
 
-const Wrapper = styled("div")`
+const Wrapper = styled('div')`
   display: block;
   margin: 0;
   padding: 0;
 `;
 
 const options = [
-  { label: "Twoje konto", path: "/account" },
-  { label: "Kantor", path: "/exchange" },
-  { label: "Bonusy", path: "/powerups" },
-  { label: "Osiągnięcia", path: "/achievements" },
-  { label: "Społeczność", path: "/social" },
+  { label: 'Twoje konto', path: '/account' },
+  { label: 'Kantor', path: '/exchange' },
+  { label: 'Bonusy', path: '/powerups' },
+  { label: 'Osiągnięcia', path: '/achievements' },
+  { label: 'Społeczność', path: '/social' }
 ];
 
 const Menu = ({ isVisible, onClose }) => {
-  const [token, setToken] = useCookie("token", "");
+  const [token, setToken] = useCookie('token', '');
 
-  const logout = () => setToken("");
+  const logout = () => setToken('');
 
-  return (<>
-    <MenuBox isVisible={isVisible}>
-      <Layout>
-        <Wrapper>
-          <IconButton
-            src={cross}
-            width="25"
-            height="25"
-            alt="Ukryj menu"
-            onClick={onClose}
-          />
-        </Wrapper>
-        <MenuUl>
-          {options.map(({ label, path }) => (
-            <li key={path}>
-              <MenuLink to={path}>
-                <Wrapper>{label}</Wrapper>
-              </MenuLink>
-            </li>
-          ))}
-        </MenuUl>
-        <Wrapper>
-          <Button tertiary>
-            Wyloguj
-          </Button>
-        </Wrapper>
-      </Layout>
-    </MenuBox>
-    <Overlay isVisible={isVisible} onClick={onClose} />
-  </>);
+  return (
+    <>
+      <MenuBox isVisible={isVisible}>
+        <Layout>
+          <Wrapper>
+            <IconButton
+              src={cross}
+              width="25"
+              height="25"
+              alt="Ukryj menu"
+              onClick={onClose}
+            />
+          </Wrapper>
+          <MenuUl>
+            {options.map(({ label, path }) => (
+              <li key={path}>
+                <MenuLink to={path}>
+                  <Wrapper>{label}</Wrapper>
+                </MenuLink>
+              </li>
+            ))}
+          </MenuUl>
+          <Wrapper>
+            <Button tertiary>Wyloguj</Button>
+          </Wrapper>
+        </Layout>
+      </MenuBox>
+      <Overlay isVisible={isVisible} onClick={onClose} />
+    </>
+  );
 };
 
 export default Menu;
