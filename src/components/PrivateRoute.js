075fb2171bfb2acfa9fc0b72c155bf93e8b0cdf6
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2018-11-25 07:21:08 +0100

    redirect to home from private route if not authenticated

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
 import { Route, Redirect } from "react-router-dom";
+import useUser from "../hooks/useUser";
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
+  const user = useUser();
+  if (!user) {
+    return <Redirect to='/'></Redirect>;
+  }
+
   return <Route {...rest} render={props => <Component {...props} />} />;
 };
 
