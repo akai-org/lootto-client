d2bf7342b00fa3c3f34a11b5c2ecfb7ec36ca6eb
Poe <piotr.a.ptak@icloud.com>
2018-11-25 10:52:02 +0100

    mikolaj powiedzial dopiero teraz

--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -1,27 +1,27 @@
-import React, { Fragment, useContext } from "react";
-import Layout from "../components/Layout";
-import PropTypes from "prop-types";
-import Logo from "../components/Logo";
-import Button from "../components/Button";
-import Particles from "../components/Particles.js";
-import Astronaut from "../components/Astronaut";
-import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
-import { Redirect } from "react-router-dom";
-import useCookie from "../hooks/useCookie";
-import useUser from "../hooks/useUser";
+import React, { Fragment, useContext } from 'react';
+import Layout from '../components/Layout';
+import PropTypes from 'prop-types';
+import Logo from '../components/Logo';
+import Button from '../components/Button';
+import Particles from '../components/Particles.js';
+import Astronaut from '../components/Astronaut';
+import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
+import { Redirect } from 'react-router-dom';
+import useCookie from '../hooks/useCookie';
+import useUser from '../hooks/useUser';
 
-import UserContext from "../contexts/UserContext";
-import { authorizedRequest } from "../utils/request";
+import UserContext from '../contexts/UserContext';
+import { authorizedRequest } from '../utils/request';
 
 export default function LoginScreen(props) {
   const { history, onLogin } = props;
-  const [token, setToken] = useCookie("token", "");
-  const [tutorialCompleted] = useCookie("tutorialCompleted", false);
+  const [token, setToken] = useCookie('token', '');
+  const [tutorialCompleted] = useCookie('tutorialCompleted', false);
 
   const user = useUser();
 
   if (user) {
-    return <Redirect to='/game' />
+    return <Redirect to="/game" />;
   }
 
   return (
@@ -35,23 +35,26 @@ export default function LoginScreen(props) {
           subtitle="Witamy w grze miejskiej wykorzystującej goelokalizację, inspirowanej Lotto, w której wygrasz prawdziwe pieniądze i będziesz się świetnie bawić z przyjaciółmi!"
         />
         <FacebookLogin
-          appId="352388892232894"
+          appId={process.env.FACEBOOK_APP_ID}
           autoLoad={true}
           fields="name,email,picture"
           isMobile={false}
           callback={response => {
             if (response.accessToken) {
               setToken(response.accessToken);
-              authorizedRequest('auth', { method: 'POST', body: {
-                access_token: response.accessToken
-              }}).then(user => {
-                  onLogin(user);
-                  if (tutorialCompleted) {
-                    history.push("/game");
-                  } else {
-                    history.push("/tutorial");
-                  }
-                });
+              authorizedRequest('auth', {
+                method: 'POST',
+                body: {
+                  access_token: response.accessToken
+                }
+              }).then(user => {
+                onLogin(user);
+                if (tutorialCompleted) {
+                  history.push('/game');
+                } else {
+                  history.push('/tutorial');
+                }
+              });
             }
           }}
           render={renderProps => (
